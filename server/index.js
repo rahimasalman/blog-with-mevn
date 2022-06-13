4ba9e5329a13c2ccf49ea98cdd7f5d9c015f8f20
rahimasalman <rehimesalman7@gmail.com>
2022-06-13 23:36:33 +0400

    add listening port

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+// import router from './router/routes.js';
+
 // express config
 const app = express();
 app.use(cors());
@@ -10,3 +12,17 @@ app.use(express.json());
 
 // enviroment variables
 dotenv.config();
+
+// router
+// app.use('/posts', router);
+
+
+// listen on port
+app.listen(process.env.PORT, () => {
+   mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   })
+      .then(console.log(`Server is running on port ${process.env.PORT}`)
+      .catch(err => console.log(err)));
+})
