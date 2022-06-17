25db8806aeb36cbf339db28ce4ede14bc890c73b
rahimasalman <rehimesalman7@gmail.com>
2022-06-18 01:51:47 +0400

    add create post functionality

---
+++ client/src/components/PostForm.vue
@@ -0,0 +1,76 @@
+/* eslint-disable */
+<template>
+  <div class="container mt-5">
+    <form @submit.prevent="submitForm">
+      <div class="field column is-centered">
+        <label class="label">Title</label>
+        <div class="control">
+          <input
+            v-model="post.title"
+            class="input"
+            type="text"
+            placeholder="Title"
+          />
+        </div>
+      </div>
+
+      <div class="field column is-centered">
+        <label class="label">Content</label>
+        <div class="control">
+          <textarea
+            v-model="post.content"
+            class="textarea"
+            placeholder="Content here"
+          ></textarea>
+        </div>
+      </div>
+
+      <div class="field column is-centered">
+        <label class="label">Creator</label>
+        <div class="control">
+          <input
+            v-model="post.creator"
+            class="input"
+            type="text"
+            placeholder="Creator"
+          />
+        </div>
+      </div>
+
+      <div class="field column is-centered">
+        <label class="label">Email</label>
+        <div class="control has-icons-left has-icons-right">
+          <input
+            v-model="post.email"
+            class="input"
+            type="email"
+            placeholder="Email"
+          />
+          <span class="icon is-small is-left">
+            <i class="fa fa-envelope" aria-hidden="true"></i>
+          </span>
+          <span class="icon is-small is-right"> </span>
+        </div>
+      </div>
+
+      <div class="field column is-centered is-grouped">
+        <div class="control">
+          <button type="submit" class="button is-success">Submit</button>
+        </div>
+        <div class="control">
+          <router-link to="/" class="button is-outline is-light"
+            >Cancel</router-link
+          >
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["post", "submitForm"],
+};
+</script>
+
+<style></style>
--- client/src/views/CreateView.vue
+++ client/src/views/CreateView.vue
@@ -1,7 +1,50 @@
 <template>
-  <div></div>
+  <post-form :post="post" :submitForm="createPost" />
 </template>
 <script>
-export default {};
+import PostForm from "../components/PostForm.vue";
+import { reactive } from "vue";
+import { useRouter } from "vue-router";
+export default {
+  components: {
+    PostForm,
+  },
+  setup() {
+    const API_URL = "http://localhost:5000/posts";
+    const router = useRouter();
+    const post = reactive({
+      title: "",
+      content: "",
+      creator: "",
+      email: "",
+    });
+
+    async function createPost() {
+      try {
+        const response = await fetch(API_URL, {
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+          },
+          body: JSON.stringify({
+            title: post.title,
+            content: post.content,
+            creator: post.creator,
+            email: post.email,
+          }),
+        });
+        //eslint-disable-next-line no-unused-vars
+        const json = await response.json();
+        router.push("/");
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    return {
+      post,
+      createPost,
+    };
+  },
+};
 </script>
 <style></style>
