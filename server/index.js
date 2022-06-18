10a26ecfcf2d80d801afeeb957abd5a8b739d9c9
rahimasalman <rehimesalman7@gmail.com>
2022-06-18 14:26:07 +0400

    Revert "fix port" commit no need to deploy this
    This reverts commit 5da67dba2d44cb7e8d001c89a31a6aa5b3eb6887.

--- server/index.js
+++ server/index.js
@@ -19,12 +19,11 @@ app.use('/posts', router);
 
 
 // listen on port
-const port = process.env.PORT || 5000;
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
    mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
    })
-      .then(console.log('Server is running on `${port}`'))
+      .then(console.log('Server is running on port 5000'))
       .catch(err => console.log(err));
 });
